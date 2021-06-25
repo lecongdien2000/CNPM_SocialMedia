ad9552eb4c41b7751ddae46782ab2dfd283b66db
lecongdien2000 <73303456+lecongdien2000@users.noreply.github.com>
2021-06-25 16:27:54 +0700

    up

--- code/database_sql/Database_Social.sql
+++ code/database_sql/Database_Social.sql
@@ -1,6 +1,5 @@
-CREATE DATABASE database_social;
-use database_social;
-CREATE TABLE User (
+use ckpO7pCTY8;
+CREATE TABLE user (
     userID VARCHAR(10) NOT NULL,
     fullName VARCHAR(30) NOT NULL,
 		username VARCHAR(15) NOT NULL,
@@ -9,49 +8,49 @@ CREATE TABLE User (
 		PRIMARY KEY (userID)
 );
 
-CREATE TABLE Post (
+CREATE TABLE post (
     postID VARCHAR(10) NOT NULL,
     userID VARCHAR(10) NOT NULL,
 		text VARCHAR(500),
 		dateCreated DATETIME NOT NULL,
 		PRIMARY KEY (postID),
-		FOREIGN KEY (userID) REFERENCES User(userID)
+		FOREIGN KEY (userID) REFERENCES user(userID)
 );
 
-CREATE TABLE Media (
+CREATE TABLE media (
     postID VARCHAR(10) NOT NULL,
     mediaPath VARCHAR(100) NOT NULL,
 		PRIMARY KEY (postID ,mediaPath),
-		FOREIGN KEY (postID) REFERENCES Post(postID)
+		FOREIGN KEY (postID) REFERENCES post(postID)
 );
 
 
-CREATE TABLE Liked (
+CREATE TABLE liked (
     postID VARCHAR(10) NOT NULL,
 		userID VARCHAR(10) NOT NULL,
 		PRIMARY KEY (postID,userID),
-		FOREIGN KEY (postID) REFERENCES Post(postID),
-		FOREIGN KEY (userID) REFERENCES User(userID)
+		FOREIGN KEY (postID) REFERENCES post(postID),
+		FOREIGN KEY (userID) REFERENCES user(userID)
 );
 
-CREATE TABLE Shared (
+CREATE TABLE shared (
     taggedPostID VARCHAR(10) NOT NULL,
 		postID VARCHAR(10) NOT NULL,
 		PRIMARY KEY (taggedPostID,postID),
-		FOREIGN KEY (postID) REFERENCES Post(postID)
+		FOREIGN KEY (postID) REFERENCES post(postID)
 );
 
-CREATE TABLE Comment (
+CREATE TABLE comment (
     commentID VARCHAR(10) NOT NULL,
 		userID VARCHAR(10) NOT NULL,
 		postID VARCHAR(10) NOT NULL,
 		text VARCHAR(500) NOT NULL,
 		dateCreated DATETIME,
 		PRIMARY KEY (commentID),
-		FOREIGN KEY (userID) REFERENCES User(userID),
-		FOREIGN KEY (postID) REFERENCES Post(postID)
+		FOREIGN KEY (userID) REFERENCES user(userID),
+		FOREIGN KEY (postID) REFERENCES post(postID)
 );
 
-
+CREATE TABLE data ( `path` VARCHAR(500) NOT NULL , `data` LONGBLOB NOT NULL ) ENGINE = InnoDB;
 
 INSERT INTO user VALUES ("0", "Nguyễn Văn Nam", "abc", "123456", "2020-03-28");
